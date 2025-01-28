49914666fe40923c90001c301bfdfdceae341014
Abdujalilov-Abdurahim <abdujalilovabdurahim72@gmail.com>
2025-01-28 18:29:39 +0500

    Update bootstrap.php
    pass

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,7 +2,7 @@
 
 require_once "autoload.php";
 
-$conn = new Database("127.0.0.1", "oop-teach","root", "Abdurahim_777");
+$conn = new Database("127.0.0.1", "oop-teach","root", "");
 $pdo = $conn->connect();
 
 Post::$pdo = $pdo;
